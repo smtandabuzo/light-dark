a3bee455926468d56d4194576f52298cf80640d1
Sazi <s_mtandabuzo@yahoo.com>
2021-07-21 09:05:18 +0200

    Dark light mode

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark mode style
+function darkMode(){
+    nav.style.backgroundColor = 'rgba(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+}
+
+// Light mode style
+function lightMode(){
+
+}
+
 
 // Switch theme dynamically
 function switchTheme(event){
-    console.log(event.target.checked);
+    if(event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+    }
 }
 
 // Event Listener
