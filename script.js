4c9d040a61a2f7424f772a48254900fe738c6350
Sazi <s_mtandabuzo@yahoo.com>
2021-07-24 08:00:48 +0200

    Finish up project

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
+const image1 = document.getElementById('img1');
+const image2 = document.getElementById('img2');
+const image3 = document.getElementById('img3');
 const textBox = document.getElementById('text-box');
+
+// Dark or light images
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
 
 // Dark mode style
 function darkMode(){
     nav.style.backgroundColor = 'rgba(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
+    imageMode('dark');
 }
 
 // Light mode style
 function lightMode(){
-
+    nav.style.backgroundColor = 'rgba(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    imageMode('light');
 }
 
 
@@ -22,12 +37,21 @@ function lightMode(){
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local storage for current theme
+const currentTheme = localStorage.getItem('theme');
+
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+}
